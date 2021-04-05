887a2e40dd45c607eea4bd2e8d85fa2ec5bccfb0
sandeepshakya2019 <sandeepshakya2015@gmail.com>
2021-04-05 16:44:06 +0530

    Types and Data in query GraphQL

--- server.js
+++ server.js
@@ -9,16 +9,31 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
 // Middleware "/graphql" is an endpoint (only one endpoint)
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput{
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
         type RootQuery{
-            events:[String!]!
+            events:[Event!]!
         }
         type RootMutation{
-            createEvent(name:String):String
+            createEvent(eventInput: EventInput):Event
         }
 
         schema{
@@ -29,11 +44,19 @@ app.use(
     rootValue: {
       // Resolvers same name as query and mutation
       events: () => {
-        return ["Cooking", "Seelling", "Coding"];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random.toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+          //   date: new Date().toISOString(),
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
