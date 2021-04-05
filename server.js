7b48255e04aff47b34bbf5a908f03b0ada3001f3
sandeepshakya2019 <sandeepshakya2015@gmail.com>
2021-04-05 15:51:39 +0530

    Adding the .env file

--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 // const graphqlHttp = require("express-graphql");
 // const { buildSchema } = require("graphql");
+dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
 
-//Middleware "/graphql" is an endpoint (only one endpoint)
+// Middleware "/graphql" is an endpoint (only one endpoint)
 // app.use(
 //   "/graphql",
 //   graphqlHttp({
 //     schema: buildSchema(`
+//         type RootQuery{
+//             event:[String!]!
+//         }
+//         type RootMutation{
+//             createEvent(name:String):String
+//         }
+
 //         schema{
-//             query:,
-//             mutation:
+//             query: RootQuery,
+//             mutation RootMutation:
 //         }
 //     `),
 //     rootValue: {},
 //   })
 // );
-app.listen(8000, () => {
+
+app.get("/", (req, res) => {
+  res.send("Sandeep Shakya");
+});
+const port = process.env.PORT;
+
+app.listen(port, () => {
   console.log(":) Server Connect Successfully ");
 });
